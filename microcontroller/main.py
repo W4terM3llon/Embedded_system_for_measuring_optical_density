c651157e7d5964fedd9938b6f52af67f215da35c
Adrian Maciejewski <adma@nisportal.com>
2024-01-06 14:24:08 +0100

    working reading for Monday exercises

--- microcontroller/main.py
+++ microcontroller/main.py
@@ -6,23 +6,27 @@ a1_pin.atten(ADC.ATTN_11DB)
 a1_pin.width(ADC.WIDTH_10BIT)
 
 pow2_16 = 2**16-1
-a2_pin = PWM(Pin(33), freq=4_000, duty_u16=0)
+a2_pin = PWM(Pin(33), freq=10_000, duty_u16=0)
 
-def saveReading(data):
-    with open("sensorReadings.csv", 'a') as f:
-        f.write(data)
-        f.write("\n")
 
-duty = 0
-while True:
-    a2_pin.duty_u16(duty)
-    reading = a1_pin.read_uv()
-    saveReading(reading)
-    print(reading)
+over1000Counter = 0
+with open('data222.txt','w') as file:
+    duty = 0
+    while True:
+        a2_pin.duty_u16(duty)
+        reading = a1_pin.read()
+        print(reading)
 
-    duty = duty+100
-    time.sleep(0.1)
-    if duty > pow2_16:
-        break
+        duty = duty+10
+
+        file.write(str(reading))
+        file.write(', ')
+        time.sleep(0.005)
+        
+        if reading >= 1000:
+            over1000Counter+=1
+        
+        if reading >= 1023 and over1000Counter > 100:
+            break
 
 
