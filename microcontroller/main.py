375f42e7478b4c4238def25fe1b80d234a8876f5
Adrian Maciejewski <adma@nisportal.com>
2024-01-08 10:45:12 +0100

    working corerct version!

--- microcontroller/main.py
+++ microcontroller/main.py
@@ -1,32 +1,39 @@
 from machine import Pin, ADC, PWM
 import time
-
-a1_pin = ADC(25)
-a1_pin.atten(ADC.ATTN_11DB)
-a1_pin.width(ADC.WIDTH_12BIT)
+import constants as cnst
 
 pow2_16 = 2**16-1
 pow2_12 = 2**12-1
-a2_pin = PWM(Pin(33), freq=4_000_000, duty_u16=0)
 
-dutyIncrement=50
-over1000Counter = 0
-with open('data5.txt','w') as file:
-    duty = 0
-    while True:
-        a2_pin.duty_u16(duty)
-        reading = a1_pin.read_uv()
-        print(reading)
+class Measurement:
+    def __init__(self, duty, freq):
+        self.duty = duty
+        self.freq = freq
+        
+        self.a1_pin = ADC(25)
+        self.a1_pin.atten(ADC.ATTN_11DB)
+        self.a1_pin.width(ADC.WIDTH_12BIT)
 
-        duty = duty+dutyIncrement
+        self.a2_pin = PWM(Pin(33), freq=self.freq, duty=self.duty)
 
-        file.write(f"{reading}, {duty}\n")
+    def measure(self):
+        self.a2_pin.duty(self.duty)
+        self.a2_pin.freq(self.freq)
+
+        reading = self.a1_pin.read_uv()
+        scaledReading = (reading - cnst.minDutyReadUv) / (cnst.maxDutyReadUv - cnst.minDutyReadUv)
+        print(scaledReading)
+
+        file.write(f"{scaledReading}, {self.duty}\n")
         time.sleep(0.0001)
-        
-        if reading >= pow2_12:
-            over1000Counter+=1
-        
-        if reading >= pow2_12 and over1000Counter > 100:
-            break
+
+freq = {1: 200_000, 2: 200_000, 3: 10_000, 4: 100_000, 5: 4_000_000}
+for i in range(1, 2): # change which measurements are done
+    measurement = Measurement(0, freq[i])
+    with open(f'data{i}.txt','w') as file:
+        for duty in range(1023):
+            measurement.duty = duty
+            measurement.measure()
+
 
 
