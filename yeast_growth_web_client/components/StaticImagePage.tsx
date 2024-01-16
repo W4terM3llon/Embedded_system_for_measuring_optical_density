568c056e2469048096f5f1fa9d14c8d032429fcd
Adrian Maciejewski <adma@nisportal.com>
2024-01-16 16:50:19 +0100

    working frontend autoupdate and more

--- yeast_growth_web_client/components/StaticImagePage.tsx
+++ yeast_growth_web_client/components/StaticImagePage.tsx
@@ -3,60 +3,72 @@ import {
   Button,
   Container,
   FormControl,
+  FormControlLabel,
+  FormLabel,
   InputLabel,
   MenuItem,
   Select,
+  Switch,
 } from "@mui/material";
 import { useState, useEffect, useMemo, useCallback } from "react";
 import { blob } from "stream/consumers";
-import downloadSvg from '../public/download-svgrepo-com.svg'
-
-type MeasurementFileNamesType =
-{
-  measurementFileNames: string[];
-}
+import downloadSvg from "../public/download-svgrepo-com.svg";
 
 export function StaticImagePage() {
-  const [measurementFileNames, setMeasurementFileNames] = useState<string[]>([]);
+  const [measurementFileNames, setMeasurementFileNames] = useState<string[]>(
+    []
+  );
   const [data, setData] = useState<any>(null);
   const [isLoading, setLoading] = useState<any>(true);
+  const [isAutoUpdate, setIsAutoUpdate] = useState<boolean>(false);
+  const [chosenMeasurementFileName, setChosenMeasurementFileName] =
+    useState<string>("");
 
-  const [chosenMeasurementFileName, setChosenMeasurementFileName] = useState<string>("");
-  
-  const onFileNameChosen = useCallback(
-    (measurementFileName: string) => {
-      setChosenMeasurementFileName(measurementFileName)
+  const [count, setCount] = useState(0);
 
-      fetch(
-        "http://127.0.0.1:5000/api/v1/yeastGrowthPlot?" +
-          new URLSearchParams({ id: measurementFileName }),
-        {
-          method: "GET",
-        }
-      )
-        .then((response) => response.blob())
-        .then((blob) => {
-          setData(blob);
-          setLoading(false);
-        })
-        .catch((x) => console.log(x));
-    },
-    [],
-  )
-  
-  useEffect(() => {
+  const fetchData = useCallback((measurementFileName: string) => {
     fetch(
-      "http://127.0.0.1:5000/api/v1/measurementFileNames",
+      "http://127.0.0.1:5000/api/v1/yeastGrowthPlot?" +
+        new URLSearchParams({ id: measurementFileName }),
       {
         method: "GET",
       }
     )
+      .then((response) => response.blob())
+      .then((blob) => {
+        setData(blob);
+        setLoading(false);
+      })
+      .catch((x) => console.log(x));
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCount(count + 1);
+      if (isAutoUpdate && chosenMeasurementFileName) {
+        fetchData(chosenMeasurementFileName);
+      }
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [chosenMeasurementFileName, count, fetchData]);
+
+  const onFileNameChosen = useCallback(
+    (measurementFileName: string) => {
+      setChosenMeasurementFileName(measurementFileName);
+      fetchData(measurementFileName);
+    },
+    [fetchData]
+  );
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/v1/measurementFileNames", {
+      method: "GET",
+    })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         setMeasurementFileNames(data.measurementFileNames);
-        if (data.measurementFileNames.length > 0)
-        {
+        if (data.measurementFileNames.length > 0) {
           onFileNameChosen(data.measurementFileNames[0]);
         }
       })
@@ -77,42 +89,84 @@ export function StaticImagePage() {
       <Container
         sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
       >
-        {!isLoading ? (<><Box sx={{ height: "600px", width: "600px", display: "flex" }}>
-          {url ? (<img
-            style={{ height: "100%", width: "100%", objectFit: "contain" }}
-            src={`${url}`} />) : (<p>Please choose a measurement</p>)}
-        </Box>
-        <FormControl  variant="standard" fullWidth sx={{width: "600px"}}>
-          <Container
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: 'center',
-              width: "100%",
-            }}
-          >
-            <div></div>
-            <div>
-              <Select
-                labelId="Measurement-label"
-                id="Measurement"
-                value={chosenMeasurementFileName}
-                label="Measurement"
-                onChange={(e) => {onFileNameChosen(e.target.value)}}
+        {!isLoading ? (
+          <>
+            <Box sx={{ height: "600px", width: "600px", display: "flex" }}>
+              {url ? (
+                <img
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    objectFit: "contain",
+                  }}
+                  src={`${url}`}
+                />
+              ) : (
+                <p>Please choose a measurement</p>
+              )}
+            </Box>
+            <FormControl variant="standard" sx={{ width: "600px" }}>
+              <Container
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  width: "100%",
+                }}
               >
-                {measurementFileNames.map((measurement) => (<MenuItem key={measurement} value={measurement}>{measurement}</MenuItem>))}
-              </Select>
-            </div>
-            <div>
-              <a href={"http://127.0.0.1:5000/api/v1/yeastGrowthPlot?" + new URLSearchParams({ id: chosenMeasurementFileName })}>
-              <img
-                style={{ height: "2rem", width: "2rem", objectFit: "contain"}}
-                src={downloadSvg.src}
-              />
-              </a>
-            </div>
-          </Container>
-        </FormControl></>) : <p>Loading...</p>}
+                <div>
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        value={isAutoUpdate}
+                        onChange={(e) => {
+                          setIsAutoUpdate(e.target.checked);
+                        }}
+                      />
+                    }
+                    label="Auto update"
+                  />
+                </div>
+                <div>
+                  <Select
+                    labelId="Measurement-label"
+                    id="Measurement"
+                    value={chosenMeasurementFileName}
+                    label="Measurement"
+                    onChange={(e) => {
+                      onFileNameChosen(e.target.value);
+                    }}
+                  >
+                    {measurementFileNames.map((measurement) => (
+                      <MenuItem key={measurement} value={measurement}>
+                        {measurement}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </div>
+                <div>
+                  <a
+                    href={
+                      "http://127.0.0.1:5000/api/v1/yeastGrowthPlot?" +
+                      new URLSearchParams({ id: chosenMeasurementFileName })
+                    }
+                  >
+                    <img
+                      style={{
+                        height: "2rem",
+                        width: "2rem",
+                        objectFit: "contain",
+                      }}
+                      src={downloadSvg.src}
+                    />
+                  </a>
+                </div>
+              </Container>
+            </FormControl>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
       </Container>
     </Container>
   );
