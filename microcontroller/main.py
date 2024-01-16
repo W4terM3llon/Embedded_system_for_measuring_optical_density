2ded4b7ff11b368f7705b24bda66deaa6945cbe7
Adrian Maciejewski <adma@nisportal.com>
2024-01-16 19:55:50 +0100

    working sending frmo microcontroler

--- microcontroller/main.py
+++ microcontroller/main.py
@@ -4,9 +4,12 @@ from measurement import Measurement
 from blinker import Blinker
 from sensorLed import SensorLed
 from loopController import LoopController
+from DataSender import DataSender
         
 
 def run():
+    SHOULD_SEND_DATA = True
+
     measurement = Measurement(15)
     blinker = Blinker(26)
 
@@ -16,9 +19,11 @@ def run():
     loopController.addObserver(blinker)
     while loopController.isRunning:
         loopController.terminateIfButtonPressed()
+        if not loopController.isRunning:
+            break
 
         currentTime = time.ticks_ms()
-        measurement.readAndSaveWhenTimeIsRight(currentTime)
+        measurement.readAndSaveWhenTimeIsRight(currentTime, SHOULD_SEND_DATA)
         blinker.blinkIfTimeIsRight(currentTime)
 
 if __name__ == "__main__":
--- server/app/controller/RecordYeastGrowthHandler.py
+++ server/app/controller/RecordYeastGrowthHandler.py
@@ -1,4 +1,3 @@
 def HandleRecordYeastGrowth(fileName, OD600, time):
     with open(f'./measurements/{fileName}', 'a') as f:
-        print(f'./measurements/{fileName}')
         f.write(f'{OD600}, {time}, {-1}\n')
